312731182b059f30a06ccd69e8cef6dfedd562ab
falak50 <83400991+falak50@users.noreply.github.com>
2023-08-17 13:06:25 -1200

    35

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ff from './assets/h'
+const Home = () => {
+    return (
+        <div>
+            <h1>this is home</h1>
+        </div>
+    );
+};
+
+export default Home;
